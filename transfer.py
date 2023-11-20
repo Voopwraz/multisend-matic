e5223108679bd08b95f31f40be49cb3eb2008439
Voopwraz <104880061+Voopwraz@users.noreply.github.com>
2023-11-20 23:47:53 +0500

    transfer.py

--- transfer.py
+++ transfer.py
@@ -1,94 +1,114 @@
-import os
-import time
+import threading
+from web3 import Web3
 import requests
-import numpy as np
-from bs4 import BeautifulSoup
-from web3 import Web3, Account
+import time
+import random
 
-rpc_url = 'https://polygon-pokt.nodies.app'
-private_keys_file = 'accs.txt'
-send_to_file = 'send.txt'
-# Начальное и конечное значения для minimum_balance, а также шаг
-min_balance_start = 1.0  # минимальный баланс для отправки, начальное значение
-min_balance_end = 1.2  # минимальный баланс для отправки, конечное значение
-min_balance_step = 0.01  # минимальный баланс для отправки, шаг
-minimum_extra_balance = 0.5 # кол-во матика минимального для отправки на другой кошелек
-matic_token_contract_address = "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0"
+# ваш токен API Polygon
+POLYGON_API_TOKEN = 'you_api'
 
-erc20_abi = """[{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"setParent","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"bytes","name":"sig","type":"bytes"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"bytes32","name":"data","type":"bytes32"},{"internalType":"uint256","name":"expiration","type":"uint256"},{"internalType":"address","name":"to","type":"address"}],"name":"transferWithSig","outputs":[{"internalType":"address","name":"from","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"withdraw","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"user","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"deposit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_childChain","type":"address"},{"internalType":"address","name":"_token","type":"address"}],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"parent","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"parentOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"currentSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"bytes32","name":"hash","type":"bytes32"},{"internalType":"bytes","name":"sig","type":"bytes"}],"name":"ecrecovery","outputs":[{"internalType":"address","name":"result","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"networkId","outputs":[{"internalType":"bytes","name":"","type":"bytes"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"EIP712_TOKEN_TRANSFER_ORDER_SCHEMA_HASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"name":"disabledHashes","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"tokenIdOrAmount","type":"uint256"},{"internalType":"bytes32","name":"data","type":"bytes32"},{"internalType":"uint256","name":"expiration","type":"uint256"}],"name":"getTokenTransferOrderHash","outputs":[{"internalType":"bytes32","name":"orderHash","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"CHAINID","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"EIP712_DOMAIN_HASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"EIP712_DOMAIN_SCHEMA_HASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"input1","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"output1","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"input1","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"output1","type":"uint256"}],"name":"Withdraw","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"input1","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"input2","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"output1","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"output2","type":"uint256"}],"name":"LogTransfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token","type":"address"},{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"input1","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"input2","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"output1","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"output2","type":"uint256"}],"name":"LogFeeTransfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}]"""
+# Генерация случайного значения MINIMUM_BALANCE с шагом 0.001
+MINIMUM_BALANCE = round(random.uniform(1.0, 1.2), 3)
 
-w3 = Web3(Web3.HTTPProvider(rpc_url))
-matic_token_contract = w3.eth.contract(address=Web3.to_checksum_address(matic_token_contract_address), abi=erc20_abi)
+# минимальный объем Matic в транзакции
+MINIMUM_MATIC_AMOUNT = 0.5
 
-def get_fast_gas():
-    api_key = "ВАШЕ АПИ ПОЛИГОН"  # Ваш API ключ
-    url = f"https://api.polygonscan.com/api?module=gastracker&action=gasoracle&apikey={api_key}"
+# максимальное время ожидания статуса транзакции (в секундах)
+MAX_WAIT_TIME = 120
 
-    response = requests.get(url).json()
+def get_initial_balance(private_key):
+    w3 = Web3(Web3.HTTPProvider('https://polygon-rpc.com'))
+    account = w3.eth.account.from_key(private_key)
+    balance = w3.eth.get_balance(account.address)
+    print(f"Начальный баланс кошелька {account.address}: {w3.from_wei(balance, 'ether')} Matic")
 
-    if response['status'] == '1':
-        fast_gas = float(response['result']['FastGasPrice'])
-        return w3.to_wei(fast_gas, 'gwei')
-    else:
-        raise Exception("Не удалось получить информацию о быстрой цене газа")
+def send_transaction(private_key, to_address, amount, gas_price, minimum_balance, minimum_matic_amount):
+ w3 = Web3(Web3.HTTPProvider('https://polygon-rpc.com'))
+ account = w3.eth.account.from_key(private_key)
+ 
+ balance = w3.eth.get_balance(account.address)
+ if balance < amount:
+     print(f"Недостаточно средств для отправки транзакции с кошелька {account.address}")
+     return
 
-def send_tokens(sender_private_key, receiver_address):
-    try:
-        sender_account = Account.from_key(sender_private_key)
-        print(f"Начало работы с кошельком: {sender_account.address}")
+ if amount < minimum_matic_amount:
+     print(f"Объем отправляемого Matic меньше {minimum_matic_amount}. Транзакция отменена.")
+     return
 
-        # Запрос баланса
-        sender_balance = matic_token_contract.functions.balanceOf(sender_account.address).call() / 1e18
-        print(f"Баланс кошелька {sender_account.address}: {sender_balance} MATIC")
+ nonce = w3.eth.get_transaction_count(account.address)
+ gas_limit = 21000
+ gas_price = max(gas_price, get_gas_price())
+ 
+ txn = {
+     'nonce': nonce,
+     'to': to_address,
+     'value': w3.to_wei(amount, 'ether'),
+     'gas': gas_limit,
+     'gasPrice': gas_price
+ }
 
-        if sender_balance >= (minimum_balance + minimum_extra_balance):
-            amount_to_send = sender_balance - minimum_balance
+ # Проверяем баланс перед отправкой транзакции
+ remaining_balance = w3.eth.get_balance(account.address) - w3.to_wei(amount, 'ether')
+ remaining_matic = w3.from_wei(remaining_balance, 'ether')
 
-            if amount_to_send >= minimum_extra_balance:
-                token_amount = w3.to_wei(amount_to_send, 'ether')
-                gas_price = int(w3.eth.gas_price * 1)  # Множитель можно настроить по вашему усмотрению
+ if remaining_matic < minimum_balance:
+     print(f"Оставшийся баланс меньше минимального значения {minimum_balance} Matic. Транзакция отменена.")
+     return
 
-                transaction = matic_token_contract.functions.transfer(receiver_address, token_amount).build_transaction({
-                    'from': sender_account.address,
-                    'gas': 40000,
-                    'gasPrice': gas_price,
-                    'nonce': w3.eth.get_transaction_count(sender_account.address),
-                    'chainId': 137
-                })
+ signed_txn = w3.eth.account.sign_transaction(txn, private_key)
+ txn_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction).hex()
+ 
+ print(f"Отправлена транзакция с кошелька {account.address} на адрес {to_address}, хеш: {txn_hash}")
 
-                signed_transaction = sender_account.sign_transaction(transaction)
-                tx_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
-                print(f"Отправлено {amount_to_send} MATIC с адреса {sender_account.address} на {receiver_address}, хеш транзакции: {tx_hash.hex()}")
-        else:
-            print("Недостаточно средств для отправки токенов")
-    except ValueError:
-        print(f"Неверный приватный ключ: {sender_private_key}")
+ start_time = time.time()
+ while time.time() - start_time < MAX_WAIT_TIME:
+     if get_transaction_status(txn_hash):
+         print(f"Транзакция {txn_hash} успешно подтверждена.")
+         break
+     else:
+         print(f"Ожидание подтверждения транзакции {txn_hash}...")
+         time.sleep(5)
+
+ return txn_hash
+
+def get_transaction_status(txn_hash):
+    w3 = Web3(Web3.HTTPProvider('https://polygon-rpc.com'))
+    txn_receipt = w3.eth.get_transaction_receipt(txn_hash)
+    return txn_receipt['status'] == 1 if txn_receipt else False
+
+def get_gas_price():
+  headers = {'Authorization': f'Bearer {POLYGON_API_TOKEN}'}
+  try:
+      response = requests.get('https://gasstation.polygon.technology/v2', headers=headers)
+      response.raise_for_status()
+      gas_price_dict = response.json()
+      if isinstance(gas_price_dict['fast'], dict) and 'maxFee' in gas_price_dict['fast']:
+          return int(gas_price_dict['fast']['maxFee'])
+      else:
+          print(f"Некорректные данные о стоимости газа: {gas_price_dict['fast']}")
+          return 0 # Возвращаем 0 в случае ошибки
+  except requests.exceptions.RequestException as e:
+      print(f"Ошибка при получении стоимости газа: {e}")
+      return 0 # Возвращаем 0 в случае ошибки
+
+def read_from_file(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f.readlines()]
 
 def main():
-    with open(private_keys_file, 'r') as f:
-        private_keys = f.read().splitlines()
+    private_keys = read_from_file('accs.txt')
+    to_addresses = read_from_file('send.txt')
 
-    with open(send_to_file, 'r') as f:
-        receiver_addresses = f.read().splitlines()
+    threads = int(input('Введите количество потоков (от 1 до 5): '))
 
-    if len(private_keys) != len(receiver_addresses):
-        print("Количество адресов в файлах не совпадает.")
-        return
+    for i in range(min(threads, len(private_keys))):
+        private_key = private_keys[i]
+        to_address = to_addresses[i]
+        amount = 1  # Укажите желаемое количество валюты Matic
+        gas_price = get_gas_price()
 
-    for i in range(len(private_keys)):
-        sender_private_key = private_keys[i]
-        receiver_address = receiver_addresses[i]
-
-        if not w3.is_address(receiver_address):
-            print(f"Неверный адрес получателя: {receiver_address}")
-            continue
-
-        try:
-            send_tokens(sender_private_key, receiver_address)
-        except Exception as e:
-            print(f"Ошибка при отправке токенов: {e}")
-
-        time.sleep(5)
+        get_initial_balance(private_key)
+        threading.Thread(target=send_transaction, args=(private_key, to_address, amount, gas_price, MINIMUM_BALANCE, MINIMUM_MATIC_AMOUNT)).start()
 
 if __name__ == '__main__':
     main()
